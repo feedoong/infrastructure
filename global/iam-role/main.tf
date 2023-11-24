16206c84f0566a785bfd462d35f92ff3b9b7ffe0
HoJun Jang <bigfanoftim@gmail.com>
2023-11-24 23:46:32 +0900

    feat: codedeploy 관련 인프라 수정
    API 서버에 맞춰 기존 80 포트 SG를 8080 포트로 변경
    code_deploy -> codedeploy로 리소스명 수정
    codedeploy 대상이 되는 EC2 instance가 S3에 대한 엑세스가 필요해 S3FullAccess IAM Role 연결

--- global/iam-role/main.tf
+++ global/iam-role/main.tf
@@ -30,3 +30,51 @@ resource "aws_iam_role_policy_attachment" "codedeploy_role_attachment" {
   role       = aws_iam_role.code_deploy.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
+
+#######################################################
+### IAM Role for EC2
+#######################################################
+resource "aws_iam_role" "codedeploy_ec2_instance_role" {
+  name = "codedeploy_ec2_instance_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "codedeploy_ec2_instance_policy" {
+  name        = "codedeploy_policy"
+  description = "A policy for CodeDeploy permissions"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+        ],
+        Effect = "Allow",
+        Resource = "*"
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_policy_attach" {
+  role       = aws_iam_role.codedeploy_ec2_instance_role.name
+  policy_arn = aws_iam_policy.codedeploy_ec2_instance_policy.arn
+}
+
+resource "aws_iam_instance_profile" "codedeploy_instance_profile" {
+  name = "codedeploy_instance_profile"
+  role = aws_iam_role.codedeploy_ec2_instance_role.name
+}
